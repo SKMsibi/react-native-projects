927d18067e3a5ee5b6e82807eefedbf7d5e58794
skmsibi <sabelokabelomsibiskm@gmail.com>
2018-05-31 10:08:35 +0200

    added for the game of life project

---
+++ the-game-of-life/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome to the Game of life (the mobile version)</Text>
+        <Text>Changes you make will automatically reload.</Text>
+      <Text>No shaking your phone please.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
